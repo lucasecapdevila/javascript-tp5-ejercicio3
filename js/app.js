8e6f124007ce826ea2332be0d586ea35554f79f2
Lucas Capdevila <lcapdevila60@gmail.com>
2023-11-25 00:49:11 -0300

    se agregan funciones para agregar y quitar tareas de la lista

---
+++ js/app.js
@@ -0,0 +1,49 @@
+//  Variables
+const form = document.querySelector('form')
+const input = document.querySelector('input')
+const btnAgregar = document.getElementById('botonAgregar')
+const h2 = document.querySelector('h2')
+const ul = document.getElementById('listaTareas')
+const sinTareas = document.getElementById('vacio')
+
+
+
+//  Funciones
+btnAgregar.addEventListener('click', (e) => {
+  e.preventDefault()
+  const tarea = input.value
+
+  //  Corre el código solo si el usuario NO pasa valores en blanco
+  if(tarea !== ''){
+    h2.classList.remove('d-none')
+    const li = document.createElement('li')
+    li.classList.add('list-group-item', 'list-group-item-warning', 'd-flex', 'justify-content-between', 'align-items-center')
+    li.innerText = tarea
+    li.appendChild(agregarBotonEliminar())
+    ul.classList.add('list-group', 'list-group-flush', 'border', 'border-warning', 'rounded-2', 'w-25', 'mx-auto')
+    ul.appendChild(li)
+    form.reset()
+    sinTareas.classList.add('d-none')
+  } else{
+    alert('Agrega un elemento')
+  }
+})
+
+const agregarBotonEliminar = () => {
+  const btnEliminar = document.createElement('button')
+  btnEliminar.innerHTML = '<i class="fa-regular fa-trash-can"></i>'
+  btnEliminar.classList.add('btn', 'btn-danger')
+  btnEliminar.addEventListener('click', (e) => {
+    const item = e.target.parentElement
+    ul.removeChild(item)
+
+    //  Verificar que no hay tareas pendientes para volver a mostrar mensaje
+    const items = document.querySelectorAll('.list-group-item')
+    if(items.length === 0){
+      ul.classList.remove('border')
+      sinTareas.classList.remove('d-none')
+    }
+  })
+
+  return btnEliminar
+}
